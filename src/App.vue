acff45dd17fbf69c596e41e735b18ebccf593911
facundo <facundovigo@gmail.com>
2020-06-07 23:26:23 -0300

    Se sube codigo a deployar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="TP De BioInformatica con lucas sir y el facu"/>
   </div>
 </template>
 
